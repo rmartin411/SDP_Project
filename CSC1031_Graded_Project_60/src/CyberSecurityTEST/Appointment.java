3ca1dd974967df9c0496dfb67562d05a82a2a5d2
ChrisChanKho <46541246+ChrisChanKho@users.noreply.github.com>
2020-04-03 16:05:31 +0100

    Appointment check
    added check

--- CSC1031_Graded_Project_60/src/CyberSecurityTEST/Appointment.java
+++ CSC1031_Graded_Project_60/src/CyberSecurityTEST/Appointment.java
@@ -10,19 +10,34 @@ public class Appointment {
 	public Boolean scheduled;
 	
 	
-	public Appointment(Date appDate, BankEmployee emp, BankClient client, Boolean scheculded) {
+	public Appointment(Date appDate, BankEmployee emp, BankClient client, Boolean scheduled) {
 		
-		this.appointmentDate = appDate;
-		this.employee = emp;
-		this.client = client;
+		boolean initialized = securityManagerCheck(appDate);
 		
-		this.scheduled = false;
+		if(initialized) init (appDate, emp, client, scheduled);
+		else throw new SecurityException("Fake Date");
+		
 	}
 	
 	public void setScheduled(boolean value) {
 		this.scheduled = value;
 	}
 	
+	private boolean securityManagerCheck(Date date) {
+		Date currentDate = new Date();
+		if (date == null || date.after(currentDate)) {
+			return false;
+		}
+		return true;
+	}
+	
+	private void init(Date date, BankEmployee emp, BankClient client, Boolean scheduled) {
+		this.appointmentDate = date;
+		this.employee = emp;
+		this.client = client;
+		
+		this.scheduled = false;
+	}
 	
 
 }
