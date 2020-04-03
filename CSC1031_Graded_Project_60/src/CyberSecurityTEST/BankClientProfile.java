6ec97445a2ff1a54ecd42e7604719645b90bcc7a
ChrisChanKho <46541246+ChrisChanKho@users.noreply.github.com>
2020-04-03 16:08:42 +0100

    Added checks for profile details

--- CSC1031_Graded_Project_60/src/CyberSecurityTEST/BankClientProfile.java
+++ CSC1031_Graded_Project_60/src/CyberSecurityTEST/BankClientProfile.java
@@ -1,5 +1,31 @@
 package CyberSecurityTEST;
 
-public class BankClientProfile {
+import java.util.Date;
 
+public class BankClientProfile{
+	
+	private Date DoB;
+	private String address;
+	
+	public BankClientProfile(String name, String address, Date DoB) {
+		
+		boolean initialized = securityManagerCheck(DoB);
+		
+		if(initialized) init (DoB, address);
+		else throw new SecurityException("Fake Profile Details");
+		
+	}
+	
+	private boolean securityManagerCheck(Date DoB) {
+		Date currentDate = new Date();
+		if (DoB == null || DoB.after(currentDate)) {
+			return false;
+		}
+		return true;
+	}
+	
+	private void init(Date DoB, String address) {
+		this.DoB = DoB;
+		this.address = address;
+	}
 }
